61d5476f2c0a6e16fb5b59e05c65d3e462c399f2
quesabe <roman.vasilev@ottofeller.com>
2022-01-08 15:55:45 +0300

    added tests for require-comment-before-useeffect rule

---
+++ tests/lib/rules/require-comment-before-useeffect.js
@@ -0,0 +1,75 @@
+'use strict'
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/require-comment-before-useeffect')
+const {RuleTester } = require('eslint')
+
+//------------------------------------------------------------------------------
+// Test cases
+//------------------------------------------------------------------------------
+
+const valid = [
+  `
+  // This effect does nothing
+  useEffect(() => undefined, [])
+  `,
+  `
+  /* This effect does nothing */
+  useEffect(() => undefined, [])
+  `,
+  `
+  /**
+   * This effect does nothing
+   */
+  useEffect(() => undefined, [])
+  `,
+  `
+  // This effect does nothing
+  React.useEffect(() => undefined, [])
+  `,
+  `
+  /* This effect does nothing */
+  React.useEffect(() => undefined, [])
+  `,
+  `
+  /**
+   * This effect does nothing
+   */
+  React.useEffect(() => undefined, [])
+  `,
+]
+
+const invalid = [
+  {
+    code: 'useEffect(() => undefined, [])',
+    errors: [{messageId: 'missingComment', line: 1}],
+  },
+  {
+    code: `
+      const num = 12
+      useEffect(() => undefined, [])
+    `,
+    errors: [{messageId: 'missingComment', line: 3}],
+  },
+  {
+    code: 'React.useEffect(() => undefined, [])',
+    errors: [{messageId: 'missingComment', line: 1}],
+  },
+  {
+    code: `
+      const num = 12
+      React.useEffect(() => undefined, [])
+    `,
+    errors: [{messageId: 'missingComment', line: 3}],
+  },
+]
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({parserOptions: {ecmaVersion: 6}})
+ruleTester.run('require-comment-before-useeffect', rule, {valid, invalid})
